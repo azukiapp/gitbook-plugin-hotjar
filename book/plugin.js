f3bee54fdf4b28d6930dd85b910a483aebe0b4a2
Marcus Gadbem <mcsgad@gmail.com>
2015-12-08 19:07:46 -0200

    Fixing hooks to the final gitbook version (2.6.4)

--- book/plugin.js
+++ book/plugin.js
@@ -1,13 +1,3 @@
 require(["gitbook"], function(gitbook) {
-    // Load Hotjar scripts
-    gitbook.events.bind("start", function(e, config) {
-      (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:cfg.hjid,hjsv:5};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-      })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-    });
+  // ...
 });
