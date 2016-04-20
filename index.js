97ab88d02861decdb5dd2edfb8eb862b12cc2580
Johan Preynat <johan.preynat@gmail.com>
2016-04-20 14:59:50 +0200

    Include Hotjar tracking code using GitBook 3 templating

--- index.js
+++ index.js
@@ -3,26 +3,6 @@ module.exports = {
     assets: "./book",
     js: [
       "plugin.js"
-    ],
-    html: {
-      "body:end": function() {
-        var config = this.options.pluginsConfig.hotjar || {};
-
-        if (!config.hjid) {
-          throw "Hotjar: option 'hjid' is required.";
-        }
-
-        return ""
-          + "<!-- Hotjar Tracking Code -->"
-          + "<script>(function(h,o,t,j,a,r){"
-          + "h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};"
-          + "h._hjSettings={hjid:"+config.hjid+",hjsv:5};"
-          + "a=o.getElementsByTagName('head')[0];"
-          + "r=o.createElement('script');r.async=1;"
-          + "r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;"
-          + "a.appendChild(r);"
-          + "})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');</script>";
-      }
-    }
+    ]
   }
 };
